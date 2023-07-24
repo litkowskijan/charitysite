d3f849d13889d67b5761cfe5fa51fde311d284c4
Jan Litkowski <janlitkowski01@gmail.com>
2023-07-24 20:25:12 +0200

    contact form functionality

--- src/LandingPageComponents/Contact/Contact.jsx
+++ src/LandingPageComponents/Contact/Contact.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Decoration from '../../Assets/Decoration.svg';
-import { IoLogoFacebook, IoLogoInstagram } from 'react-icons/io'
+import { IoLogoFacebook, IoLogoInstagram } from 'react-icons/io';
 
 const Contact = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const formData = {
+          email,
+          name,
+          message,
+        };
+    
+        try {
+            const response = await fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+
+          const data = await response.json();
+          console.log('Odpowiedź z API:', data.status === 'success');
+    
+          setEmail('');
+          setName('');
+          setMessage('');
+        } catch (error) {
+          console.error('Błąd wysyłki formularza:', error);
+        }
+      };
+
     return (
         <div className='contact__section section' id='contact'>
             <div className="contact__container container">
@@ -12,17 +46,17 @@ const Contact = () => {
                     <div className="contact__form">
                         <div className='contact__form__1row'>
                             <div className='contact__name__part'>
-                                <label for='inputName'>Wpisz swoje imię</label>
-                                <input type='text' id='inputName' placeholder='Krzysztof'></input>
+                                <label htmlFor='inputName'>Wpisz swoje imię</label>
+                                <input type='text' id='inputName' placeholder='Krzysztof' value={name} onChange={(e) => setName(e.target.value)}></input>
                             </div>
                             <div className='contact__email__part'>
-                                <label for='inputEmail'>Wpisz swój email</label>
-                                <input type='email' id='inputEmail' placeholder='abc@xyz.pl'></input>
+                                <label htmlFor='inputEmail'>Wpisz swój email</label>
+                                <input type='email' id='inputEmail' placeholder='abc@xyz.pl' value={email} onChange={(e) => setEmail(e.target.value)}></input>
                             </div>
                         </div>
-                        <label for='inputMessage'>Wpisz swoją wiadomość</label>
-                        <textarea id='inputMessage' placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'></textarea>
-                        <button className='contact__form__button' type='submit'>Wyślij</button>
+                        <label htmlFor='inputMessage'>Wpisz swoją wiadomość</label>
+                        <textarea id='inputMessage' value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'></textarea>
+                        <button className='contact__form__button' type='submit' onClick={handleSubmit}>Wyślij</button>
                     </div>
                 </div>
             </div>
